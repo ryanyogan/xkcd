016d8060bafe35d9640f021a0c11349b3205b359
Ryan Yogan <ryanyogan@gmail.com>
2015-02-06 18:15:28 -0600

    Added the basic gulpfile to get rocking

---
+++ Gulpfile.js
@@ -0,0 +1,17 @@
+var gulp       = require('gulp');
+var browserify = require('gulp-browserify');
+
+// We need to compile all of our source down into on nice file
+// we are using brwoserify, however reactify is going to take care
+// of our JSX transpiling (is that a word?)
+gulp.task('scripts', function() {
+  gulp.src(['app/main.js'])
+    .pipe(browserify({
+      debug: true,
+      transform: ['reactify']
+    }))
+    .pipe(gulp.dest('./public'));
+});
+
+// The default task $ gulp, will fire off scripts
+gulp.task('default', ['scripts']);
